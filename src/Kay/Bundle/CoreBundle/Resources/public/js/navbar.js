2b4bd4b125ba9b007eda29c45199340cf0495a0b
Jerome Lelievre <jerome.lelievre2@gmail.com>
2017-04-03 15:58:38 +0200

    Add navbar css and js animation

--- src/Kay/Bundle/CoreBundle/Resources/public/js/navbar.js
+++ src/Kay/Bundle/CoreBundle/Resources/public/js/navbar.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
+    if ($(document).scrollTop() > 70) {
         $('nav').addClass('shrink');
     } else {
         $('nav').removeClass('shrink');
